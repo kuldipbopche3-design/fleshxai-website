5a8a0af2332ed57aab1093229c144b03684a021c
Kuldip Bopche <kuldipbopche3@gmail.com>
2026-07-05 23:39:58 +0530

    Update vite base path to relative to prevent blank screen on Netlify/Vercel root domains

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/AI-Voice-Agent-Wabsight/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
